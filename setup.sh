722ff5c5ea62e998216f488e8fb35e6dc05cac23
Oliver Switzer <oliver@geometer.io>
2021-11-19 14:46:04 -0500

    Start version controlling coc-settings.json and .gitconfig

--- setup.sh
+++ setup.sh
@@ -1,21 +1,29 @@
 set -e
 
-copy_dotfile() {
+repo_dir=$(pwd)
+
+symlink_dotfile() {
   file_name=$1
-  current_dir=$(pwd)
+  dest_path=$2
 
-  repo_path=$current_dir/$file_name
-  home_path=$HOME/$file_name
+  repo_dotfile=$repo_dir/$file_name
+  dest_dotfile=$dest_path/$file_name
 
-  if test -f "$home_path"; then
-    echo "⚠️  $home_path already exists, copying it to $file_name.bak"
-    cp -n $home_path{,.bak}
+  if test -f "$dest_dotfile"; then
+    echo "⚠️  $dest_dotfile already exists, copying it to $file_name.bak"
+    cp -n $dest_dotfile{,.bak}
   fi
 
-  ln -sf $repo_path $home_path
+  echo "Symlinking $repo_dotfile to $dest_dotfile..."
+  ln -sf $repo_dotfile $dest_dotfile
 }
 
-copy_dotfile '.zshrc'
-copy_dotfile '.vimrc'
-copy_dotfile '.tmux.conf'
+symlink_dotfile '.zshrc' $HOME
+symlink_dotfile '.vimrc' $HOME
+symlink_dotfile '.tmux.conf' $HOME
+symlink_dotfile '.gitconfig' $HOME
 
+[ -d ~/.config/nvim/ ] \
+  && symlink_dotfile 'coc-settings.json' $HOME/.config/nvim
+
+mkdir ~/.tmux && git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
