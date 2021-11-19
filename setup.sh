2a7c3c7d8ba7a5c0f03781a6430040cb23751256
Oliver Switzer <oliver@geometer.io>
2021-11-19 15:07:26 -0500

    Install brew deps as a part of setup.sh

--- setup.sh
+++ setup.sh
@@ -20,30 +20,58 @@ symlink_dotfile() {
 
 function install_brew_dep() {
   formula=$1
+  optional_cask_flag=$2
 
-  if brew ls --versions $2 $formula > /dev/null; then
+  if brew ls --versions $optional_cask_flag $formula > /dev/null; then
     echo "✅ '$formula' is already installed"
   else
     echo "'$formula' is not installed"
     echo "⏳ Installing '$formula'"
     HOMEBREW_NO_AUTO_UPDATE=1 brew install $2 $formula
+    brew install $2 $formula
   fi
 }
 
 
-# Install brew dpes
+## Brew Dependencies
+
+#### Binaries
+
 install_brew_dep 'neovim'
 install_brew_dep 'tmux'
 install_brew_dep 'upterm'
+
+#### Applications
+
+# Work-related
 install_brew_dep 'docker' --cask
+install_brew_dep 'dash' --cask
+install_brew_dep 'slack' --cask
+install_brew_dep 'tandem' --cask
+install_brew_dep 'iterm2' --cask
+install_brew_dep '1password' --cask
+install_brew_dep 'postman' --cask
+install_brew_dep 'zoom' --cask
+install_brew_dep 'visual-studio-code' --cask
 
-mkdir ~/.tmux && git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# Utilities
+install_brew_dep 'stats' --cask
+install_brew_dep 'flycut' --cask
+install_brew_dep 'shiftit' --cask
 
-# Copy over dotfiles
+# Non-work related
+install_brew_dep 'flux' --cask
+install_brew_dep 'vlc' --cask
+install_brew_dep 'spotify' --cask
+
+## Dotfiles
+
 symlink_dotfile '.zshrc' $HOME
 symlink_dotfile '.vimrc' $HOME
 symlink_dotfile '.tmux.conf' $HOME
 symlink_dotfile '.gitconfig' $HOME
+
+mkdir ~/.tmux && git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # Coc Config
 mkdir -p ~/.config/nvim
