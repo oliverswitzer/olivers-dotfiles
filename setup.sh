5187e974eca42b52a22832b8dc50ffc065f51bde
Oliver Switzer <oliver@geometer.io>
2021-11-28 22:57:33 -0500

    Install asdf and powerlevel10k in setup.sh

--- setup.sh
+++ setup.sh
@@ -40,6 +40,14 @@ function install_brew_dep() {
 install_brew_dep 'neovim'
 install_brew_dep 'tmux'
 install_brew_dep 'upterm'
+install_brew_dep 'postgresql'
+install_brew_dep 'asdf'
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install oh-my-zsh plugin for terminal UI: powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 #### Applications
 
