0528b221710eb636c212756b1e8ec603d987ce83
Oliver Switzer <oliver@geometer.io>
2022-06-21 10:42:57 -0400

    Add note to change syntax in handlers.lua when upgrading to vim 0.8

--- nvim/lua/user/lsp/handlers.lua
+++ nvim/lua/user/lsp/handlers.lua
@@ -83,6 +83,7 @@ end
 
 M.on_attach = function(client, bufnr)
   if client.name == "tsserver" then
+    --Change to client.server_capabilities.documentFormattingProvider = false after upgrading to vim 0.8
     client.resolved_capabilities.document_formatting = false
   end
   lsp_keymaps(bufnr)
