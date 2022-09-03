e72f0a4d45f9b387a0a3574ed6b018f3460712b1
Oliver Switzer <oliver@geometer.io>
2022-09-03 17:53:24 -0400

    Don't write all files when running a test due to weird orphaned buffer issue that often happens due to dadbod plugin

--- nvim/lua/user/tests.lua
+++ nvim/lua/user/tests.lua
@@ -3,9 +3,9 @@ vim.cmd("let test#strategy = 'vimux'")
 local opts = { noremap = true, silent = true }
 local keymap = vim.api.nvim_set_keymap
 
-keymap("n", "<C-t><C-n>", ":wa <bar> TestNearest<CR>", opts)
-keymap("n","<C-t><C-f>", ":wa <bar> TestFile<CR>", opts)
-keymap("n","<C-t><C-s>", ":wa <bar> TestSuite<CR>", opts)
-keymap("n","<C-t><C-l>", ":wa <bar> TestLast<CR>", opts)
-keymap("n","<C-t><C-t>", ":wa <bar> TestLast<CR>", opts)
-keymap("n","<C-t><C-g>", ":wa <bar> TestVisit<CR>", opts)
+keymap("n", "<C-t><C-n>", ":w <bar> TestNearest<CR>", opts)
+keymap("n","<C-t><C-f>", ":w <bar> TestFile<CR>", opts)
+keymap("n","<C-t><C-s>", ":w <bar> TestSuite<CR>", opts)
+keymap("n","<C-t><C-l>", ":w <bar> TestLast<CR>", opts)
+keymap("n","<C-t><C-t>", ":w <bar> TestLast<CR>", opts)
+keymap("n","<C-t><C-g>", ":w <bar> TestVisit<CR>", opts)
